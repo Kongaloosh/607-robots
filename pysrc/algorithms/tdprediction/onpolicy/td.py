fde888e5136407c92d87b8918400ca5458c12360
Alex <konga.kearney@gmail.com>
2015-06-22 14:37:26 -0600

    made TD PEP8 for readability

--- pysrc/algorithms/tdprediction/onpolicy/td.py
+++ pysrc/algorithms/tdprediction/onpolicy/td.py
@@ -8,28 +8,27 @@ import numpy as np
 import pylab as pl
 from pysrc.algorithms.tdprediction.tdprediction import TDPrediction
 
+
 class TD(TDPrediction):
-  '''
-  classdocs
-  '''
+    """ classdocs """
 
-  def __init__(self, config):
-    '''
-    Constructor
-    '''
-    self.nf     = config['nf']
-    self.th     = np.zeros(self.nf)
-    self.z      = np.zeros(self.nf)
-    self.alpha  = config['alpha']
-        
-  def initepisode(self):
-    self.z = np.zeros(self.nf)
-    
-  def step(self, params):
-    phi=params['phi']; R=params['R']; phinext=params['phinext']
-    g=params['g']; l=params['l']; gnext=params['gnext']
-    delta = R + gnext*np.dot(phinext,self.th) - np.dot(phi, self.th)
-    self.z = g*l*self.z + phi
-    self.th += self.alpha*delta*self.z
+    def __init__(self, config):
+        """    Constructor """
+        self.nf = config['nf']
+        self.th = np.zeros(self.nf)
+        self.z = np.zeros(self.nf)
+        self.alpha = config['alpha']
 
-      
+    def initepisode(self):
+        self.z = np.zeros(self.nf)
+
+    def step(self, params):
+        phi = params['phi']
+        r = params['R']
+        phinext = params['phinext']
+        g = params['g']
+        l = params['l']
+        gnext = params['gnext']
+        delta = r + gnext*np.dot(phinext, self.th) - np.dot(phi, self.th)
+        self.z = g*l*self.z + phi
+        self.th += self.alpha*delta*self.z
