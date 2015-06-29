76d5161ddd935b87848d6a9875784527da38ca1c
Alex <konga.kearney@gmail.com>
2015-06-29 13:56:11 -0600

    added an algorithm configuration file

---
+++ results/robot-experiments/anns_experiment/td/configalg.py
@@ -0,0 +1,13 @@
+"""specifies the configuration for the algorithms"""
+import numpy as np
+import cPickle as pickle
+
+# alphas = 10**np.arange(-3, 0.1, .25)                                         #values for alpha sweep-over
+# lambdas = np.concatenate((np.arange(0, .9, .1), np.arange(.9, 1.01, .01)))    #values of lambda to sweep over
+#
+# configs = [{'alpha': alpha, 'lmbda': lm} for alpha in alphas for lm in lambdas]
+configs = [{'alpha': 0.01, 'lambda': 0.9}]
+
+f = open('configalg.pkl', 'wb')
+
+pickle.dump(configs, f)
