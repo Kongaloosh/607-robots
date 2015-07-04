dd39c72cd3a1186bd317d470c8c4bacc0af2540a
Alex <konga.kearney@gmail.com>
2015-07-03 19:52:52 -0600

    more config-file work

--- results/robot-experiments/anns_experiment/utotd/configalg.py
+++ results/robot-experiments/anns_experiment/utotd/configalg.py
@@ -3,7 +3,7 @@ import numpy as np
 import cPickle as pickle
 
   
-# etas      = np.concatenate(([0], 10**np.arange(-6, 0.1, 0.25)))  # start = -6, stop =0.1, step 0.25
+# etas      = np.concatenate(([0], 10**np.arange(-6, 0.1, 0.25)))
 # initds    = 10**np.arange(0, 3.1, .25)
 # ratios    = np.array([0, 0.001, 0.01, 0.1, 1.] )
 # lms       = np.concatenate((np.arange(0, .9, .1), np.arange(.9, 1.01, .01)))
@@ -20,6 +20,8 @@ import cPickle as pickle
 #                  for beta in betas
 #                  for lm in lms
 #               ]
+#
+# f = open('configalg.pkl', 'wb')
 
 configs = [{'eta': 1, 'initd': 1, 'beta': np.array([0]), 'lmbda': 0.5}]
 f = open('configalg.pkl', 'wb')
