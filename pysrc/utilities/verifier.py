7e8b6cd52b2bde9508a2bd7d95bb89abaab516d9
Alex <konga.kearney@gmail.com>
2015-07-05 21:27:48 -0600

    decided to turf the verifier to build a better post-hoc one from scratch. Am updating the relevant experiments and problems.

--- pysrc/utilities/verifier.py
+++ pysrc/utilities/verifier.py
@@ -1,53 +1,14 @@
-import numpy as np
+"""A re-work of Craig's verifier which is a re-work of my verifier."""
 
-class Verifier(object):
 
-    def __init__(self, gamma, instantaneous=False):
-        self.instantaneous = instantaneous
-        self.horizon = self.timesteps_from_gamma(gamma)
-        
-        # using numpy instead of deque as we need to do calculations on the data rather
-        # than iterate of the individual items
-        if not self.instantaneous:
-            self.reward_history = np.zeros(self.horizon)
-        else:
-            self.reward_history = None
-            
-        self.prediction_history = np.zeros(self.horizon)
-        
-        self.count = 0
-
-    def _calculate_reward(self):
-        ret_val = None
-        if self.instantaneous:
-            ret_val = self.reward_history
-        else:
-            # in the cumulative 
-            ret_val = np.sum(self.reward_history)
-            
-        return ret_val
-        
-    def update(self, reward, prediction):
-        if self.instantaneous:
-            self.reward_history = reward
-        else:
-            self.append_to_np_array(self.reward_history, reward)
-        
-        ret_val = None
-        
-        if self.count == self.horizon:
-            ret_val = self._calculate_reward() - self.prediction_history[0]
-            
-        self.append_to_np_array(self.prediction_history, prediction)
-        
-        self.count = min(self.count + 1, self.horizon)
-            
-        return ret_val
-            
-    def timesteps_from_gamma(self, gamma):
-        return 1/(1-gamma)
-
-    @staticmethod
-    def append_to_np_array(array, val):
-        array[0:-1] = array[1:]
-        array[-1] = val
+def verifier(config, obs, reward):
+    """ provided a config containing"""
+    gamma = config['gamma']
+    safe_horizon = 2*(1/(1-gamma))    # abstract out the '2' to some config (experiment?)
+    terminal = len(obs)-safe_horizon            # we only compute the return to a specific safe point
+    ret = []
+    for i in range(terminal):                   # for each time-step to our terminal
+        ret_i = 0
+        for idx, s in enumerate(obs):              # for all the received rewards
+            ret_i = s[reward] * (gamma**idx)       # calculate our return
+        ret.append(ret_i)                          # append the val
