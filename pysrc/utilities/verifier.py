2e89d06b696bf48f58088cf76a279e46c28d4da8
Alex <konga.kearney@gmail.com>
2015-07-06 13:16:09 -0600

    commented the verifier for easy-reading

--- pysrc/utilities/verifier.py
+++ pysrc/utilities/verifier.py
@@ -1,4 +1,7 @@
-"""A re-work of Craig's verifier which is a re-work of my verifier."""
+"""
+A collection of methods which post-hoc calculate return for a particular experiment file
+
+"""
 import math
 import pickle
 from matplotlib import pyplot
@@ -12,22 +15,23 @@ def calculate_discounted_return(config, obs, reward_calculator):
         of the end of perception.
     """
     gamma = config['gamma']
-    safe_horizon = int(math.floor(2*(1/(1-gamma))))    # abstract out the '2' to some config (experiment?)
-    terminal = len(obs)-safe_horizon            # we only compute the return to a specific safe point
+    safe_horizon = int(math.floor(2*(1/(1-gamma))))     # abstract out the '2' to some config (experiment?)
+    terminal = len(obs)-safe_horizon                    # we only compute the return to a specific safe point
     ret = []
-    for i in range(terminal):                   # for each time-step to our terminal
-        if i % 14 == 0:
-            j = i
-            ret_i = 0
-            if i % 1000 == 0 and i > 0:
-                print("{i} of {n}".format(i=i, n=terminal))
-            while j < len(obs):              # for all the received rewards
-                ret_i += reward_calculator.get_reward(obs[j]) * (gamma**(j-i))  # calculate our return
-                j += 1
-            ret.append(ret_i)                          # append the val
-    pickle.dump(ret, open("verifier", "wb"))
-    pyplot.plot(ret)
-    pyplot.show()
+    for i in range(terminal):                           # for each time-step to our terminal
+        j = i                                           # inner loop start index
+        ret_i = 0                                       # set the value for the current time step's return
+
+        if i % 1000 == 0 and i > 0:                     # pretty print
+            print("{i} of {n}".format(i=i, n=terminal))
+
+        while j < len(obs):                             # for all the received rewards from i onwards
+            # add the reward of a time step discounted by gamma relative to where it is in relation to i
+            ret_i += reward_calculator.get_reward(obs[j]) * (gamma**(j-i))
+            j += 1
+
+        ret.append(ret_i)                               # append the calculated value
+    return ret                                          # the calculated return for all time steps within safe range
 
 
 def calculate_discounted_return_horizon(config, obs, reward_calculator):
@@ -38,21 +42,22 @@ def calculate_discounted_return_horizon(config, obs, reward_calculator):
         of the end of perception.
     """
     gamma = config['gamma']
-    horizon = int(math.floor(1/(1-gamma)))    # abstract out the '2' to some config (experiment?)
-    terminal = len(obs)-(2 * horizon)            # we only compute the return to a specific safe point
+    horizon = int(math.floor(1/(1-gamma)))      # abstract out the '2' to some config (experiment?)
+    terminal = len(obs)-(2 * horizon)           # we only compute the return to a specific safe point
     ret = []
     for i in range(terminal):                   # for each time-step to our terminal
-        j = i
-        ret_i = 0
-        if i % 1000 == 0 and i > 0:
+        j = i                                   # inner loop start index
+        ret_i = 0                               # set the value for the current time step's return
+
+        if i % 1000 == 0 and i > 0:             # pretty print
             print("{i} of {n}".format(i=i, n=terminal))
-        while j < i+horizon:              # only calculate a horizon's length away.
+
+        while j < i+horizon:                    # only calculate a horizon's length away.
             ret_i += reward_calculator.get_reward(obs[j]) * (gamma**(j-i))  # calculate our return
             j += 1
-        ret.append(ret_i)                          # append the val
-    pickle.dump(ret, open("verifier", "wb"))
-    pyplot.plot(ret)
-    pyplot.show()
+
+        ret.append(ret_i)                       # append the calculated val
+    return ret                                  # the calculated return for all time steps within safe range
 
 
 def calculate_return_horizon(config, obs, reward_calculator):
@@ -63,26 +68,27 @@ def calculate_return_horizon(config, obs, reward_calculator):
         of the end of perception.
     """
     gamma = config['gamma']
-    horizon = int(math.floor(1/(1-gamma)))    # abstract out the '2' to some config (experiment?)
-    terminal = len(obs)-2*horizon            # we only compute the return to a specific safe point
-    rewards = []
-    ret = []
-    for i in obs:
+    horizon = int(math.floor(1/(1-gamma)))              # abstract out the '2' to some config (experiment?)
+    terminal = len(obs)-2*horizon                       # we only compute the return to a specific safe point
+    rewards = []                                        # where each time-step's reward is stored
+    ret = []                                            # the list of return values for each time-step
+
+    for i in obs:                                       # this isn't needed, as we're not using sum
         rewards.append(reward_calculator.get_reward(i))
 
-    for i in range(terminal):                   # for each time-step to our terminal
-        j = i
-        ret_i = 0
-        if i % 1000 == 0 and i > 0:
-            print("{i} of {n}".format(i=i, n=terminal))
-        while j <= i + horizon:
-            ret_i += rewards[j]
-            j += 1
-        ret.append(ret_i)
-    pickle.dump(ret, open("verifier", "wb"))
-    pyplot.plot(ret)
-    pyplot.show()
+    for i in range(terminal):                           # for each time-step to our terminal
+        j = i                                           # start index of inner loop
+        ret_i = 0                                       # initialization for current time-step's return
 
+        if i % 1000 == 0 and i > 0:                     # pretty print
+            print("{i} of {n}".format(i=i, n=terminal))
+
+        while j <= i + horizon:                         # from i to the horizon
+            ret_i += rewards[j]                         # sum the value of the rewards
+            j += 1
+
+        ret.append(ret_i)
+    return ret                                          # return the list of cumulative reward from each time-step
 
 
 def calculate_return_total(config, obs, reward_calculator):
@@ -93,19 +99,18 @@ def calculate_return_total(config, obs, reward_calculator):
         of the end of perception.
     """
     gamma = config['gamma']
-    safe_horizon = int(math.floor(2*(1/(1-gamma))))    # abstract out the '2' to some config (experiment?)
-    terminal = len(obs)-safe_horizon            # we only compute the return to a specific safe point
+    safe_horizon = int(math.floor(2*(1/(1-gamma))))     # abstract out the '2' to some config (experiment?)
+    terminal = len(obs)-safe_horizon                    # we only compute the return to a specific safe point
     rewards = []
     ret = []
-    for i in obs:
+    for i in obs:                                       # extract all the rewards so we can use sum()
         rewards.append(reward_calculator.get_reward(i))
 
-    for i in range(terminal):                   # for each time-step to our terminal
-        if i % 1000 == 0 and i > 0:
+    for i in range(terminal):                           # for each time-step to our terminal
+
+        if i % 1000 == 0 and i > 0:                     # pretty print
             print("{i} of {n}".format(i=i, n=terminal))
-        ret.append(sum(rewards))
-        rewards.pop()
-        # print("{i} is value {ret_i}".format(i=i, ret_i=ret_i))
-    pickle.dump(ret, open("verifier", "wb"))
-    pyplot.plot(ret)
-    pyplot.show()
+
+        ret.append(sum(rewards))                        # sum all the values
+        rewards.pop()                                   # remove the head so that we progress to the next time-step
+    return ret
