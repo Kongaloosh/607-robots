3d9854f779a74c5c5898e6c6ce11cd670cd9d413
Alex <konga.kearney@gmail.com>
2015-07-05 22:47:00 -0600

    really super inefficient. Just hacked this up to make a graph and check something.

--- pysrc/utilities/verifier.py
+++ pysrc/utilities/verifier.py
@@ -1,14 +1,20 @@
 """A re-work of Craig's verifier which is a re-work of my verifier."""
+import math
 
 
-def verifier(config, obs, reward):
+def verifier(config, obs, reward_calculator):
     """ provided a config containing"""
     gamma = config['gamma']
-    safe_horizon = 2*(1/(1-gamma))    # abstract out the '2' to some config (experiment?)
+    safe_horizon = int(math.floor(2*(1/(1-gamma))))    # abstract out the '2' to some config (experiment?)
     terminal = len(obs)-safe_horizon            # we only compute the return to a specific safe point
     ret = []
     for i in range(terminal):                   # for each time-step to our terminal
-        ret_i = 0
-        for idx, s in enumerate(obs):              # for all the received rewards
-            ret_i = s[reward] * (gamma**idx)       # calculate our return
-        ret.append(ret_i)                          # append the val
+        if i % 20 == 0:
+            j = i
+            ret_i = 0
+            if i % 1000 == 0:
+                print("{i} of {n}".format(i=i, n=terminal))
+            while j < len(obs):              # for all the received rewards
+                ret_i += reward_calculator.get_reward(obs[j]) * (gamma**(j-i))  # calculate our return
+                j += 1
+            ret.append(ret_i)                          # append the val
