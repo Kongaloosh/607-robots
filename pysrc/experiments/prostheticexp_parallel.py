c15c3a601d3c7cc72b74de00cab5e803a6161b53
Alex <konga.kearney@gmail.com>
2015-07-16 13:42:16 -0600

    too much ram was being consumed, so I've split things up into a bunch of different combinations. I'll test and be more cautious.

--- pysrc/experiments/prostheticexp_parallel.py
+++ pysrc/experiments/prostheticexp_parallel.py
@@ -1,6 +1,7 @@
 """
     Takes a specific algorithm and a specific file, runs across a config file looking at all the args.
 """
+from debian.debtags import output
 import os
 import sys
 sys.path.insert(0, os.getcwd())
@@ -12,15 +13,15 @@ from pysrc.utilities.verifier import *
 from pysrc.utilities.max_min_finder import *
 import pickle
 import time
+import multiprocessing as mp
 
 
-def runoneconfig(file_loader, alg, prob):
+def runoneconfig(file_loader, alg, prob, config, out):
     """for the specific configuration, problem, alg,"""
     obs = file_loader.step()                                    # get the next observation diction
     state = prob.step(obs)                                      # initial state
     p = []                                                      # holds the predictions
-    s = []                                                      # holds all of the rewards
-    start = time.time()                                         # experiment timer
+    s = []
     while file_loader.has_obs():                                # while we still have observations
         obs = file_loader.step()                                # get the next observation diction
         vals = prob.step(obs)                                   # state from prob
@@ -30,10 +31,14 @@ def runoneconfig(file_loader, alg, prob):
         s.append(vals['R'])                                     # record actual reward
     #     if file_loader.i % 1000 == 0:                           # pretty print
     #         print("Step: {s} of {n}".format(s=file_loader.i, n=len(file_loader.data_stream)))
-    # print("Finished: " + str((time.time()-start)/60))           # time taken for experiment
-    file_loader.reset()
-    return p, s                                                 # return the predictions and rewards
-
+    # print('finito')
+    config['prediction'] = np.array(p)
+    config['signal'] = np.array(s)
+    config['error'] = np.array(config['return']) - \
+                      config['prediction'][:len(config['return'])]
+    # print('config\'d')
+    out.put(config)                                             # return the predictions and rewards
+    # print('outputed')
 
 def main():
     """runs the experiment with commandline args"""
@@ -47,11 +52,10 @@ def main():
 
     config_prob_path = 'results/robot-experiments/{prob}/configprob.pkl'.format(prob=args.prob)
     config_prob = pickle.load(open(config_prob_path, 'rb'))   # we load a configuration file with all of the data
-
     config_alg_path = 'results/robot-experiments/{prob}/{alg}/configalg.pkl'.format(prob=args.prob, alg=args.algname)
     config_alg = pickle.load(open(config_alg_path, 'rb'))   # we load a configuration file with all of the data
 
-    file_loader = FileLoaderApprox('results/prosthetic-data/EdwardsPOIswitching_{s}{a}.txt'.format(s=args.sVal, a=args.aVal), 14)
+    file_loader = FileLoaderApprox('results/prosthetic-data/EdwardsPOIswitching_{s}{a}.txt'.format(s=args.sVal, a=args.aVal), 100)
     # file_loader = FileLoader('results/prosthetic-data/EdwardsPOIswitching_{s}{a}.txt'.format(s=args.sVal, a=args.aVal))
 
     algs = {
@@ -67,14 +71,14 @@ def main():
         'prosthetic_experiment': Prosthetic_Experiment,
         'prosthetic_experiment_with_context': Prosthetic_Experiment_With_Context,
         'biorob': Biorob2012Experiment
-    }
+        }
 
     f = open('results/robot-experiments/{prob}/{alg}/{name}_{s}_{a}.dat'.format(prob=args.prob, alg=args.algname, s=args.sVal, a=args.aVal, name=args.filename), 'wb')
 
     # calculate the return
     calculated_return = calculate_discounted_return_backwards(config_prob,
-            file_loader.data_stream,
-            Prosthetic_Experiment)
+                                                              file_loader.data_stream,
+                                                              Prosthetic_Experiment)
     config_prob['return'] = calculated_return
 
     # calculate normalizer
@@ -85,23 +89,32 @@ def main():
     config_prob['normalizer'] = generate_normalizer(file_loader.data_stream, prob=prob)                             # get constants for normalizing states
 
     # run the experiment
-    print(len(config_alg))
+    processes = []                                  # where we define each sweep member
+    output = mp.Queue()                             # where results are stored
     for config in config_alg:                       # for the parameter sweep we're interested in
         config.update(config_prob)                  # add the problem-specific configs
         try:
             config['alpha'] /= config['num_tilings']    # divide alpha
         except:
             pass                                        # we're using an alg with different config
-        prob = problems[args.prob](config)                      # construct a problem
-        alg = algs[args.algname](config)            # build our instance of an algorithm
-        (prediction, signal) = \
-            runoneconfig(file_loader=file_loader, prob=prob, alg=alg)    # grab results of run
+        fl = FileLoaderApprox(
+            'results/prosthetic-data/EdwardsPOIswitching_{s}{a}.txt'.format(
+                s=args.sVal, a=args.aVal), 100)
+        a = algs[args.algname](config)
+        p = problems[args.prob](config)
+        processes.append(mp.Process(target=runoneconfig, args=(fl, a, p, config, output)))
 
-        config['signal'] = signal                   # adding to the config so we can save results
-        config['prediction'] = prediction
-        config['error'] = np.array(config['return']) - prediction[:len(config['return'])]
-        pickle.dump(config, f, -1)
+    print("processes: " + str(len(processes)))
 
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        print('Finished: {alg} {s} {a}'.format(alg=args.algname, s=args.sVal, a=args.aVal))
+        pickle.dump(output.get(), f, -1)
+
+    for p in processes:
+        p.join()
 
 if __name__ == '__main__':
     '''from the command-line'''
---
+++ results/robot-experiments/biorob/configprob.py
@@ -0,0 +1,26 @@
+'''
+Generates the configuration
+'''
+
+import numpy as np
+import cPickle as pickle
+from math import gamma
+
+
+def main():
+    gamma = 0.99
+    configs = {
+        # function approximation:
+        'num_tilings': 6,
+        'memory_size': 2**20,
+        'nf': 2**20,
+        'starting_element': 0,
+        'gamma': gamma
+    }
+
+
+    f = open('configprob.pkl', 'wb')
+    pickle.dump(configs, f)
+  
+if __name__ == "__main__":
+  main()  
