cfe766c3cee3b37f9a4fd6406cf4e00a25fb96ce
Alex <konga.kearney@gmail.com>
2015-07-20 14:09:58 -0600

    we find the best performance at any given comaritor parameter and return it in a list of points to be ploted

--- pysrc/plot/plot.py
+++ pysrc/plot/plot.py
@@ -1,30 +1,82 @@
 import os
 import sys
 sys.path.insert(0, os.getcwd())
-import matplotlib.pyplot as ppl
-from pysrc.plot import plot_data_process_prosthetic
+from matplotlib import pyplot
 import cPickle as pickle
-
+from itertools import chain
+import numpy
 
 def plot_performance_vs_lambda(path_prefix, label, parameters):
-    plot_file_name = path_prefix + "perfvs" + parameters[-1] + ".plot.pkl"
+    plot_file_name = path_prefix + "perf_vs_lambda" + ".plot.pkl"
     if not os.path.isfile(plot_file_name):  # if we haven't made a plot file yet
-        plot_data_process_prosthetic(parameters)
-    plotfile = file(plot_file_name, "rb")
-    data = pickle.load(plotfile)
-    # print(data[:, 2])
-    ppl.plot(data[:, 2])
-    ppl.errorbar(data[:,0], data[:,1], data[:,2], label=label)
+        plot_data_process_prosthetic(parameters, path_prefix)
+    # plotfile = file(plot_file_name, "rb")
+    # data = pickle.load(plotfile)
+    # # print(data[:, 2])
+    # ppl.plot(data[:, 2])
+    # ppl.errorbar(data[:,0], data[:,1], data[:,2], label=label)
 
 
-def plot_data_process_prosthetic(parameters):
+def plot_data_process_prosthetic(parameters, path_prefix):
+    """
+    We construct a table against a particular parameter
+    :param parameters: a dictionary as seen in main()
+    :param path_prefix: where the path goes to
+    """
+                                                                    # the different values we compare performance across
+    comparison_val = parameters['compare']                          # the name of the key for the variable we compare across
+    data = loaddata(path_prefix)                                    # we load all the runs from a trial
+    comparison_values = set([run[comparison_val] for run in data])  # we find the values we wish to vary over
+
+    plot_points = []
+    for run_parameter in comparison_values:
+        best = None
+        best_error = numpy.infty
+        for run in data:
+            new_error = sum(abs(run['error']))
+            if run[comparison_val] == run_parameter and new_error < best_error:
+                best_error = new_error
+                best = run
+        print(best_error)
+        plot_points.append(best)
+
+
+def loaddata(path, nruns = 1):
+    """
+    Takes in the number of runs---or, files we have---and looks for all the files
+    for any given file, we load all of the results and find.
+
+    Remember, the results are going to be a collection of pickled items, each
+    being one member of a sweep for a particular episode.
+
+    :param nruns: number of runs
+    :param path: the prefix to where we want to find result files
+
+    """
+    data = []
+    for run in range(1, nruns + 1):                         # for all the possible runs
+        # todo: change number of runs over file names!!!
+        filepathname = path + ".dat"                        # point to data file
+        f = open(filepathname, 'rb')                        # load the file results are stored in
+        try:                                                # While we still have sweep results in a given file
+            while True:                                     # load and append the data to our array
+                d = pickle.load(f)                          # load and append a run
+                data.append(d)
+        except EOFError:                                    # catch the end of the file to move on
+            print 'End of file reached'
+    return data
 
 
 def main():
-    path = "results/robot-experiments/biorob/"
-    postfix = 'sweep_pool_2_s1_na1'
-    if not os.path.exists(path):
-        path = "../." + path
+    """
+    Constructs a file comparing the algs across for each alpha
+    :returns none:
+    """
+    # todo: factor all of the stuff out. we should be able to tell parameters by algs
+    path = "results/robot-experiments/biorob/"          # the path to the experiments
+    postfix = 'sweep_pool_2_s1_na1'                     # the name of the experiment run
+    # if not os.path.exists(path):
+    #     path = "../." + path
 
     pathfileprefix = path+"td/"+postfix
     print("TD")
@@ -32,11 +84,12 @@ def main():
         pathfileprefix,
         "TD",
         {
-            'number_of_runs':1 ,
-            'path_prefix':pathfileprefix,
-            'number_of_parameters':2,
-            'param_1':'alpha',
-            'param_2':'lambda'
+            'number_of_runs': 1 ,
+            'path_prefix': pathfileprefix,
+            'number_of_parameters': 2,
+            'param_1': 'alpha',
+            'param_2': 'lmbda',
+            'compare': 'lmbda'
         }
     )
 
@@ -51,7 +104,8 @@ def main():
             'number_of_parameters':3,
             'param_1':'eta',
             'param_2':'initd',
-            'param_3':'lambda'
+            'param_3':'lmbda',
+            'compare': 'lmbda'
         }
     )
 
@@ -65,7 +119,8 @@ def main():
             'path_prefix':pathfileprefix,
             'number_of_parameters':2,
             'param_1':'alpha',
-            'param_2':'lambda'
+            'param_2':'lmbda',
+            'compare': 'lmbda'
         }
     )
 
@@ -75,12 +130,13 @@ def main():
         pathfileprefix,
         "UTOTD",
         {
-            'number_of_runs':1 ,
-            'path_prefix':pathfileprefix,
-            'number_of_parameters':3,
-            'param_1':'eta',
+            'number_of_runs':1,             # the number of runs in a file
+            'path_prefix':pathfileprefix,   # the prefix we use to find the data
+            'number_of_parameters':3,       # the number of parameters we compare over
+            'param_1':'eta',                # all the parameters
             'param_2':'initd',
-            'param_3':'lambda'
+            'param_3':'lmbda',
+            'compare': 'lmbda'             # the parameter we wish to compare agains
         }
     )
 
@@ -94,7 +150,8 @@ def main():
             'path_prefix':pathfileprefix,
             'number_of_parameters':2,
             'param_1':'alpha',
-            'param_2':'lambda'
+            'param_2':'lmbda',
+            'compare': 'lmbda'
         }
     )
 
@@ -109,7 +166,8 @@ def main():
             'number_of_parameters':3,
             'param_1':'eta',
             'param_2':'initd',
-            'param_3':'lambda'
+            'param_3':'lmbda',
+            'compare': 'lmbda'
         }
     )
 
@@ -117,6 +175,6 @@ def main():
     # ppl.yscale('log')
     ppl.legend()
 
-    if __name__ == '__main__':
-        main()
-    ppl.show()
+
+if __name__ == '__main__':
+    main()
