5b02d1105785764602890c5fced4f6a89849cd88
Alex <konga.kearney@gmail.com>
2015-08-11 13:06:54 -0600

    bonanza runner

---
+++ scripts/bonanza_run.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+#   This script goes over all of the algorithms and all of the values using a collection of configuration files
+# to organize a sweep to analyse performance at a variety of parameter values. The script is dependant on the
+# algorithms specified and requires the data files provided by Ann Edwards to run properly.
+#
+#   For ease of reading we first generate a set of bash scripts to be run on westgrid, then we call the first of
+# these scripts. Automatically, each script will call the next before finishing. This is setup such that we
+# baby-sit the sweep
+#
+# ====================================================================================================================
+#
+#                                          CREATE THE SCRIPTS TO RUN
+#
+# ====================================================================================================================
+
+for alg in td tdr totd autotd               # for all algorithms
+do
+for s in 1 2 3 4                            # for all subjects
+do
+for a in a na                               # for all actions in a session
+do
+for aval in 1 2 3                           # for the number of trials per person
+do
+
+var=0
+
+while  [ -f results/robot-experiments/prosthetic_experiment/$alg/configalg_$var.pkl ]; do
+# while there are still config files available for a process
+# make a config for these parameters
+
+echo '
+
+time python pysrc/experiments/prostheticexp.py s'$s' '$a$aval' prosthetic_experiment '$alg' experiment '$var' > txt/'$alg'-s'$s'-'$a$aval'-'$var'.txt
+
+if [[ '$aval' < "3" ]]
+then
+	./sh/'$alg'-s'$s'-'$a$(($aval+1))'-'$var'.sh
+	echo 'next action'
+elif [[ '$s' < "4" ]]
+then
+    ./sh/'$alg'-s'$(($s+1))'-'$a'1-'$var'.sh
+	echo 'next person'
+#else
+#    ./sh/'$alg'-s1-'$a'1-'$(($var+1))'.sh
+#    echo 'next config'
+fi
+echo done
+' > sh/$alg-s$s-$a$aval-$var.sh                           # make a script to run as a process on westgrid
+
+chmod +x sh/$alg-s$s-$a$aval-$var.sh
+
+((var=var+1))                   # increment the config number to move to the next file
+done                            # end test if file exists while
+done                            # end actions vals
+done                            # end actions
+done                            # end subjects
+done                            # end algorithms
+
+# ====================================================================================================================
+#
+#                                          RUN ALL THE SCRIPTS WE'VE CREATED
+#
+# ====================================================================================================================
+
+for alg in td                       # for all algorithms
+do
+for a in a                                         # for all actions in a session
+do
+s=1
+aval=1                                              # the first trial is where we start all our runs
+var=0
+
+while  [ -f results/robot-experiments/prosthetic_experiment/$alg/configalg_$var.pkl ]; do
+    ./sh/$alg-s$s-$a$aval-$var.sh &                 # make a script to run as a process on westgrid
+
+((var=var+1))                                       # increment the config number to move to the next file
+
+done                                                # end test if file exists while
+done                                                # end actions
+done                                                # end algorithms
+
+
