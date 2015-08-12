3ab0f7db678943eefa11675eb012d009d4848327
Alexandra Kearney <kearney@seawolf2.ibb>
2015-08-12 14:58:07 -0700

    automated the config builder for biorob and prosthetic experiment

---
+++ scripts/setup-config-biorob.sh
@@ -0,0 +1,14 @@
+module load application/python/2.7.3
+module load python/2.7.2
+
+cd results/robot-experiments/biorob/
+python configprob.py
+cd td/
+python configalg_numerical.py
+cd ../tdr
+python configalg_numerical.py
+cd ../totd
+python configalg_numerical.py
+cd ../autotd
+python configalg_numerical.py
+echo "setup configs"
