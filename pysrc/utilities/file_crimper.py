16b553c5496d78e9a004e46ed953745ef18752cb
Alex <konga.kearney@gmail.com>
2015-08-21 11:00:28 -0600

    script that auto-compresses the results

---
+++ pysrc/utilities/file_crimper.py
@@ -0,0 +1,35 @@
+"""little tool for removing un-wanted data while collating"""
+import os
+import pickle
+
+__author__ = 'alex'
+
+keys = ['alpha', 'lmbda', 'initalpha', 'error']
+
+path = 'results/robot-experiments/prosthetic_experiment/'
+exp_name = 'experiment'
+for alg in ['autotd']:
+    print("Collecting {alg}".format(alg=alg))
+    for s in ['s1','s2','s3','s4']:
+        print("Over subject {s}".format(s=s))
+        for a in ['a1','a2','a3','na1','na2','na3']:
+            print("Over trial {a}".format(a=a))
+            i = 0
+            data = path + alg + "/{name}_{s}_{a}_{i}.dat".format(name=exp_name, s=s, a=a, i=i)
+            while os.path.isfile(data):                     # if our s, a, or i values ar out of bounds, kick
+                try:
+                    f = open(data, 'r+')
+                    run_result = pickle.load(f)
+                    dump = {}
+                    for key in keys:
+                        try:
+                            dump[key] = run_result[key]
+                        except KeyError:
+                            pass
+                    f = open(data, 'wb')
+                    pickle.dump(dump,f)
+                    f.close()
+                except EOFError:
+                    print('unexpected EOF on file {i}'.format(i=i))
+                i += 1
+                data = path + alg + "/{name}_{s}_{a}_{i}.dat".format(name=exp_name, s=s, a=a, i=i)
