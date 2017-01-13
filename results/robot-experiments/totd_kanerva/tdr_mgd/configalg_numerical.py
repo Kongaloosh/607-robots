d91b6c3ad3855db68fe346ae7354edb449ff22ae
Alex Kearney <konga.kearney@gmail.com>
2017-01-13 09:35:18 -0700

    changing the config for an experiment to pass to patrick

--- results/robot-experiments/totd_kanerva/tdr_mgd/configalg_numerical.py
+++ results/robot-experiments/totd_kanerva/tdr_mgd/configalg_numerical.py
@@ -4,7 +4,8 @@ import cPickle as pickle
 
 alphas = [0.05, 0.1, 0.2, 0.3, 0.6, 1.0, 1.5, 2.0]
 lambdas = [0, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.98, 0.99, 0.995, 0.998, 0.999, 1]
-num_features = [100, 500, 1000, 2000, 5000, 10000]
+# num_features = [100, 500, 1000, 2000, 5000, 10000]
+num_features = [1024]
 configs = [{'alpha': alpha, 'lmbda': lm, 'nf': num_feature}
            for alpha in alphas for lm in lambdas for num_feature in num_features]
 print len(configs)
