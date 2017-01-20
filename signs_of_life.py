274fc102ce410e405a956e8136ddcb8577873631
Alex Kearney <konga.kearney@gmail.com>
2017-01-20 14:05:25 -0700

    pushing for virtual box

--- signs_of_life.py
+++ signs_of_life.py
@@ -5,6 +5,8 @@ import numpy as np
 
 __author__ = 'kongaloosh'
 
+central_position = 512                  # (range: 0 to 1023)
+
 serial_port = '/dev/tty.usbserial-AI03QD8V'
 serial = dynamixel.SerialStream(port=serial_port,
                                 baudrate=1000000,
@@ -18,57 +20,152 @@ for servo_id in servos:
 if not net.get_dynamixels():
     print("no servos found")
     sys.exit(0)
-else:
-    print("servos found")
-    sys.exit(0)
 
-    
-# move to original position
+print("servos found")
+print("setting register constants...")
+for actuator in net.get_dynamixels():
+    actuator._set_ccw_compliance_slope(32)
+    actuator._set_cw_compliance_slope(32)
+    actuator._set_ccw_compliance_margin(1)
+    actuator._set_cw_compliance_margin(0)
+print("controlling via encoder...")
+print("     moving to centre")
 
-# central_position = 768                  # (range: 0 to 1023)
-central_position = 512                  # (range: 0 to 1023)
+while abs(net.get_dynamixels()[0].current_position - 512) > 2 and \
+      abs(net.get_dynamixels()[1].current_position - 512) > 2:
+    for actuator in net.get_dynamixels():
+        actuator.moving_speed = 100
+        actuator.torque_enable = 1
+        actuator.torque_limit = 800
+        actuator.max_torque = 800
+        actuator.goal_position = 512
+    net.synchronize()
 
-# control encoder
-for servo_id in servos:
-    print(servo_id)
-    position = central_position
-    dynamixel.set_position(serial, servo_id, position)
-    dynamixel.send_action_packet(serial)
+# ===================================================================================================================
+#                                           Move the Servos
+# ===================================================================================================================
+
+print("     performing ballet")
+goal_pos = central_position + 10
+while goal_pos < 600:
+    while abs(net.get_dynamixels()[0].current_position - goal_pos) > 5 and \
+          abs(net.get_dynamixels()[1].current_position - goal_pos) > 5:
+        for actuator in net.get_dynamixels():
+            actuator.moving_speed = 50
+            actuator.torque_enable = 1
+            actuator.torque_limit = 700
+            actuator.max_torque = 700
+            actuator.goal_position = goal_pos
+        net.synchronize()
+    goal_pos += 20
+
+print("     moving the other way")
+goal_pos = central_position
+while goal_pos > 300:
+    while abs(net.get_dynamixels()[0].current_position - goal_pos) > 5 and \
+          abs(net.get_dynamixels()[1].current_position - goal_pos) > 5:
+        for actuator in net.get_dynamixels():
+            actuator.moving_speed = 50
+            actuator.torque_enable = 1
+            actuator.torque_limit = 700
+            actuator.max_torque = 700
+            actuator.goal_position = goal_pos
+        net.synchronize()
+    goal_pos -= 20
 
 
-# torque on an off
-for i in servos:
-    dynamixel.set_
-    dynamixel.send_action_packet(serial)
-    print(dynamixel.registers.MAX_TORQUE)
+# ===================================================================================================================
+#                                           changing torque
+# ===================================================================================================================
 
-    position = central_position + 20
-    dynamixel.set_position(serial, servo_id, position)
-    dynamixel.send_action_packet(serial)
-
-exit(0)
-
-for i in servos:
-    dynamixel.registers.TORQUE_ENABLE = True
-    dynamixel.send_action_packet(serial)
-
-
-# control trajectory of both motors (at once)
-# trajectory for one minute or longer of control
+print("Moving for 10 seconds without torque...")
 start = datetime.now()
-while (datetime.now() - start).seconds / 60 < 10:
-    if position > 700:
-        amount = -10
-    else:
-        amount = 10
+goal_pos = central_position
+while (abs(net.get_dynamixels()[0].current_position - goal_pos) > 5 and \
+       abs(net.get_dynamixels()[1].current_position - goal_pos) > 5) and \
+       (datetime.now()-start).seconds < 10:
+    for actuator in net.get_dynamixels():
+        actuator.moving_speed = 50
+        actuator.torque_enable = 0
+        actuator.torque_limit = 0
+        actuator.max_torque = 700
+        actuator.goal_position = goal_pos
+    net.synchronize()
 
-initial_position = [400, 400]
-chain.init(serial, servos, 180)
-vector = chain.make_vector(initial_position+amount, [2,3], 100)
-chain.move_to_vector(serial, vector)
-chain.wait_for_move(serial, servos)
+# ===================================================================================================================
+#                                           changing compliance
+# ===================================================================================================================
 
+# here CW means "clock-wise" and CCW means "counter clock-wise"
+print("moving 30 seconds with maxed compliance...")
+start = datetime.now()
+goal_pos = central_position
+amount = 20
+for actuator in net.get_dynamixels():
+    actuator._set_ccw_compliance_slope(254)
+    actuator._set_cw_compliance_slope(254)
+    actuator._set_ccw_compliance_margin(0)
+    actuator._set_cw_compliance_margin(1)
+    print("cw angle limit           ", actuator.cw_angle_limit)
+    print("ccw angle limit          ", actuator.ccw_angle_limit)
+    print("cw compliance margin     ", actuator.cw_compliance_margin)
+    print("ccw compliance margin    ", actuator.ccw_compliance_margin)
+    print("cw compliance slope      ", actuator.cw_compliance_slope)
+    print("ccw compliance slope     ", actuator.ccw_compliance_slope)
 
+while(datetime.now()-start).seconds < 30:
+    while (abs(net.get_dynamixels()[0].current_position - goal_pos) > 5 and \
+          abs(net.get_dynamixels()[1].current_position - goal_pos) > 5):
+        for actuator in net.get_dynamixels():
+            actuator.moving_speed = 50
+            actuator.torque_enable = 1
+            actuator.torque_limit = 700
+            actuator.max_torque = 700
+            actuator.goal_position = goal_pos
+        net.synchronize()
+    goal_pos += amount
+    if goal_pos > 700:
+        amount = -20
+    elif goal_pos < 300:
+        amount = 20
+
+print("wating for a minute with maxed compliance and 20")
+for actuator in net.get_dynamixels():
+    actuator._set_torque_enable(0)
+start = datetime.now()
+while (datetime.now()-start).seconds <60:
+    pass
+
+# ===================================================================================================================
+#                                           Minute or longer of control
+# ===================================================================================================================
+
+# resetting compliance
+for actuator in net.get_dynamixels():
+    actuator._set_ccw_compliance_slope(32)
+    actuator._set_cw_compliance_slope(32)
+    actuator._set_ccw_compliance_margin(1)
+    actuator._set_cw_compliance_margin(0)
+
+print("moving for 3 minutes...")
+start = datetime.now()
+amount = 100
+goal_pos = central_position
+while (datetime.now() - start).seconds / 60 < 3:
+    if goal_pos > 750:
+        amount = -100
+    elif goal_pos < 250:
+        amount = 100
+    while abs(net.get_dynamixels()[0].current_position - goal_pos) > 5 and \
+          abs(net.get_dynamixels()[1].current_position - goal_pos) > 5:
+        for actuator in net.get_dynamixels():
+            actuator.moving_speed = 50
+            actuator.torque_enable = True
+            actuator.torque_limit = 700
+            actuator.max_torque = 700
+            actuator.goal_position = goal_pos
+        net.synchronize()
+    goal_pos += amount
 # store the data
 # real-time plotting
 
