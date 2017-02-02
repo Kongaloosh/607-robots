c61191584ff451ad73e0c7442c027312b763b377
Alex Kearney <konga.kearney@gmail.com>
2017-02-02 10:50:19 -0700

    we now publish commands

--- scripts/robot_random_policy.py
+++ scripts/robot_random_policy.py
@@ -3,10 +3,15 @@
 import sys
 import rospy
 from beginner_tutorials.srv import robot_command
+from beginner_tutorials.msg import robot_command_state
 import random
-import time 
+import time
 
 __author__ = 'kongaloosh'
+
+pub = rospy.Publisher('robot_obeservation', robot_command_state, queue_size=10)     # publishing to
+rospy.init_node('robot_command_talker', anonymous=True)                             # initializes node with name
+
 
 def robot_command_client(x, y):
     rospy.wait_for_service('robot_controller')
@@ -14,15 +19,16 @@ def robot_command_client(x, y):
         command_service = rospy.ServiceProxy('robot_controller', robot_command)
         resp1 = command_service(x, y)
     except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+        print "Service call failed: %s" % e
+
 
 if __name__ == "__main__":
     x = 512
     y = 512
 
     while True:
-        command = random.randint(0,3)
-	print command
+        command = random.randint(0, 3)
+        print command
         if command == 0:
             x += 70
         elif command == 1:
@@ -33,5 +39,6 @@ if __name__ == "__main__":
             y -= 70
 
         robot_command_client(x, y)
-	time.sleep(0.2)
+        pub.Publish(4, command)
+        time.sleep(0.2)
     sys.exit(1)
