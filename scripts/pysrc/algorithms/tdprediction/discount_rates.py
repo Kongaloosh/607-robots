7512b989c2c9d272be7c38fef3d7ebe380944e72
Alex Kearney <konga.kearney@gmail.com>
2017-02-23 14:47:05 -0700

    adding variety of discount rates

--- scripts/pysrc/algorithms/tdprediction/discount_rates.py
+++ scripts/pysrc/algorithms/tdprediction/discount_rates.py
@@ -1,3 +1,27 @@
 __author__ = 'kongaloosh'
 
 
+def constant(gamma, data=None):
+    return gamma
+
+
+def increasing(gamma, data=None):
+    if gamma == 1:
+        return 0
+    else:
+        return gamma + 1./5.
+
+
+def end_when_stationary_2(gamma, data):
+    if data[3] is False:
+        return 1
+    else:
+        return gamma
+
+
+def end_when_stationary_3(gamma, data):
+    if data[10] is False:
+        return 1
+    else:
+        return gamma
+
