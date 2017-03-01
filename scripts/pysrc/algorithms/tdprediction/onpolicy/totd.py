d1573df19190452796de956f63a104acf0f5312a
Alex Kearney <konga.kearney@gmail.com>
2017-03-01 14:30:10 -0700

    updating TOTD to filter rupam's code to my standard

--- scripts/pysrc/algorithms/tdprediction/onpolicy/totd.py
+++ scripts/pysrc/algorithms/tdprediction/onpolicy/totd.py
@@ -1,38 +1,26 @@
-'''
-Created on May 2, 2014
-
-@author: ashique
-'''
-
 import numpy as np
 from pysrc.algorithms.tdprediction.tdprediction import TDPrediction
 
 class TOTD(TDPrediction):
-  '''
-  classdocs
-  '''
+  """classdocs"""
 
-  def __init__(self, config):
-    '''
-    Constructor
-    '''
-    self.nf       = config['nf']
-    self.th       = np.zeros(self.nf)
-    self.z        = np.zeros(self.nf)
-    self.predprev = 0.
-    try:
-        self.alpha = config['alpha'] / config['active_features']
-    except KeyError:
-        self.alpha = config['alpha']
+  def __init__(self, number_of_features, step_size, active_features=1):
+    """ Constructor """
+    self.number_of_features = number_of_features
+    self.th = np.zeros(self.number_of_features)
+    self.z = np.zeros(self.number_of_features)
+    self._last_estimate = 0.
+    self.alpha = step_size / active_features
         
   def initepisode(self):
-    self.z = np.zeros(self.nf)
+    self.z = np.zeros(self.number_of_features)
     
-  def step(self, params):
-    phi=params['phi']; R=params['R']; phinext=params['phinext']
-    g=params['g']; l=params['l']; gnext=params['gnext']
-    prednext = np.dot(phinext,self.th)
-    delta = R + gnext*prednext - self.predprev
-    self.z = g*l*self.z + self.alpha*phi - self.alpha*g*l*np.dot(self.z,phi)*phi
-    self.th += delta*self.z + self.alpha*(self.predprev - np.dot(phi, self.th))*phi
-    self.predprev = prednext
+  def step(self, phi, reward, phi_next, gamma, lmbda, gamma_next):
+    prednext = np.dot(phi_next, self.th)
+    delta = reward + gamma_next * prednext - self.predprev
+    self.z = g * lmbda * self.z + self.alpha*phi - self.alpha * gamma_next * lmbda * np.dot(self.z, phi) * phi
+    self.th += delta * self.z + self.alpha * (self.predprev - np.dot(phi, self.th)) * phi
+    self._last_estimate = prednext
+
+  def last_estimate(self):
+        return self._last_estimate
