9898d1702123be356cd0715eb1090729049f5f34
Alex Kearney <konga.kearney@gmail.com>
2017-03-08 14:47:24 -0700

    message publishing

--- scripts/td_robot_control.py
+++ scripts/td_robot_control.py
@@ -27,12 +27,12 @@ class TDRobot(object):
         self.phi = None
         self.last_estimate = 0
         self.action = None
+        self.controller_publisher = rospy.Publisher('control_publisher' + name, td_control_msg, queue_size=10)
         self.kanerva = BaseKanervaCoder(
             _startingPrototypes=self.memory_size,
             _dimensions=1,
             _numActiveFeatures=self.active_features)
         self.control = td_control
-        pub = rospy.Publisher('robot_obeservation', td_control_msg, queue_size=10)     # publishing to
         rospy.init_node('robot_command_talker', anonymous=True)                             # initializes node with name
 
     def step(self, data):
@@ -47,6 +47,10 @@ class TDRobot(object):
         self.action = action_next
         self.gamma = gnext
         # need a bottleneck to throttle things
+        self.controller_publisher.Publish(
+            reward,
+            action_next
+        )
         self.command(action_next)
 
     @staticmethod
@@ -96,7 +100,7 @@ class TDRobot(object):
         return data
 
 
-class TDRobot_Continuous(object):
+class TDRobot_continuous(object):
 
     def __init__(self, step_size, elegibility_lambda, td_control, reward_factory, gamma, gamma_factory, name=""):
         self.memory_size = 2 ** 10
@@ -109,21 +113,14 @@ class TDRobot_Continuous(object):
         self.phi = None
         self.last_estimate = 0
         self.action = None
+        self.mean_publisher = rospy.Publisher('mean_publisher' + name, td_control_msg, queue_size=10)
+        self.sigma_publisher = rospy.Publisher('sigma_publisher' + name, td_control_msg, queue_size=10)
         self.kanerva = BaseKanervaCoder(
             _startingPrototypes=self.memory_size,
             _dimensions=1,
             _numActiveFeatures=self.active_features)
         self.control = td_control
-        pub = rospy.Publisher('robot_obeservation', td_control_msg, queue_size=10)     # publishing to
         rospy.init_node('robot_command_talker', anonymous=True)                             # initializes node with name
-
-    def command(self):
-        rospy.wait_for_service('robot_controller')
-        try:
-            command_service = rospy.ServiceProxy('robot_controller', robot_command)
-            resp1 = command_service(x, y, command)
-        except rospy.ServiceException, e:
-            print "Service call failed: %s" % e
 
     def step(self, data):
         gnext = self.gamma_factory(data, self.gamma)
@@ -131,16 +128,28 @@ class TDRobot_Continuous(object):
         phi_next = self.kanerva.get_features(self.construct_obs(data))
         action_next = self.control.get_action(phi_next)
         if self.phi and self.action:
-            action = self.control.step(self.phi, reward, phi_next, self.gamma, self.lmbda, gnext)
-
+           self.control.step(self.phi, reward, phi_next, self.gamma, self.lmbda, gnext)
         self.phi = phi_next
         self.robot_command_client(self.action)
         self.action = action_next
         self.gamma = gnext
-        # reward
-        # command
         # need a bottleneck to throttle things
+        self.sigma_publisher_publisher.Publish(
+            reward,
+            action_next
+        )
+        self.command(action_next)
 
+    @staticmethod
+    def command(action):
+        rospy.wait_for_service('robot_controller')
+        x = action
+        y = 1
+        try:
+            command_service = rospy.ServiceProxy('robot_controller', robot_command)
+            resp1 = command_service(x, y, command)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s" % e
 
     @staticmethod
     def robot_command_client(command):
@@ -172,6 +181,7 @@ class TDRobot_Continuous(object):
             data.command,
         ]
         return data
+
 
 if __name__ == "__main__":
     robot = TDRobot()
