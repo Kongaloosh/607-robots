961a7d6a6b47cc24ff245a7f20e9a80e77b49216
Alex Kearney <konga.kearney@gmail.com>
2017-03-07 22:20:40 -0700

    flagging out control

---
+++ scripts/pysrc/algorithms/tdcontrol/actor_critic.py
@@ -0,0 +1,111 @@
+from pysrc.algorihtms.tdcontrol.tdcontrol import TDControl
+import numpy as np
+
+__author__ = 'kongaloosh'
+
+
+class ActorCritic(TDControl):
+    """ TD REPLACING TRACES """
+
+    def __init__(self, number_of_features, number_of_actions, step_size, active_features=1):
+        """Constructor"""
+        super(ActorCritic, self).__init__(number_of_features, number_of_actions)
+        self.z = np.zeros(self.number_of_actions, self.number_of_features)
+        self.e_critic = np.zeros()
+        self.e_actor = np.zeros()
+        self.th_critic = np.zeros(self.number_of_actions, self.number_of_features)
+        self.th_actor = np.zeros(self.number_of_actions, self.number_of_features)
+        self.step_size_actor
+        self.step_size_critic
+        self.step_size = step_size / active_features
+        self._last_estimate = None
+        self.reaward_avg = 0.
+
+    def initialize_episode(self):
+        """Initialize the episode by killing the traces"""
+        self.e_u = np.zeros(self.e_u.shape)
+        self.e_v = np.zeros(self.e_v.shape)
+
+    def actor_step(self, gamma, lmbda, phi, phi_next, critic_delta):
+        """Updates the """
+        self.actor_elegibility = gamma * lmbda * self.actor_elegibility + phi[]
+        self.actor_weights += self.actor_step_size * critic_delta * self.actor_elegibility
+
+    def step(self, phi, reward, phi_next, gamma, lmda, gamma_next):
+        critic_delta = self.critic_step(phi, reward, phi_next, gamma, lmda, gamma_next)
+        return self.actor_step(phi, phi_next, gamma, lmda, gamma_next, critic_delta)
+
+    def critic_step(self, phi, reward, phi_next):
+        """
+        :returns the critic's delta
+        """
+        action_next = np.argmax(self.np.dot(self.th_actor[L,], self.phinext))
+        delta = reward  - self.average_reward + self.gamma * np.dot(self.th_critic[:,self.action], phi_next) - np.dot(self.th_critic[], phi)
+        self.avg_reward += self.step_size_critic * delta
+        self.e_critic   = self.e_critic * self.lmbda * self.gnext  + phi
+        self.th_critic  += self.step_size_critic * delta * self.th_critic
+        return delta
+
+
+    def softmax(self, action):
+        """for a given action, returns the softmax prob"""
+        pass
+
+    def last_estimate(self):
+        return self._last_estimate
+
+
+class ContinuousActorCritic(TDControl):
+    """"""
+
+    def __init__(self, number_of_features, number_of_actions, step_size_mean, step_size_deviation, active_features):
+        """"""
+
+        self.step_size_critic = self.step_size_critic
+        self.step_size_mean = step_size_mean
+        self.step_size_deviation = step_size_deviation
+        self.active_features = active_features
+        self.number_of_features = number_of_features
+        self.number_of_actions = number_of_actions
+
+        self.e_critic    = np.zeros(self.number_of_features)
+        self.th_critic   = np.zeros(self.number_of_features)
+        self.e_mean      = np.zeros(self.number_of_features)
+        self.e_sigma     = np.zeros(self.number_of_features)
+        self.th_mean     = np.zeros(self.number_of_features)
+        self.th_sigma    = np.zeros(self.number_of_features)
+
+    def step(self, phi, reward, phi_next, gamma, lmda, gamma_next):
+        critic_delta = self.critic_step(phi, reward, phi_next, gamma, lmda, gamma_next)
+        return self.actor_step(phi, phi_next, gamma, lmda, gamma_next, critic_delta)
+
+    def critic_step(self, phi, reward, phi_next):
+        """
+        :returns the critic's delta
+        """
+        delta = reward  - self.average_reward + self.gamma * np.dot(self.th_critic, phi_next) - np.dot(self.th_critic, phi)
+        self.avg_reward += self.step_size_critic * delta
+        self.e_critic   = self.e_critic * self.lmbda * self.gnext  + phi
+        self.th_critic  += self.step_size_critic * delta * self.th_critic
+        return delta
+
+    def actor_step(self, phi, phi_next, gamma, lmbda, gamma_next, critic_delta):
+        """
+        :returns action
+        """
+        mean = np.dot(self.th_mean, phi)         # last step's mean
+        sigma = np.dot(self.th_sigma, phi)       # last step's deviation
+
+        gradient_mean = ((self.action - mean)) * phi                # gradients wrt mean and deviation
+        gradient_sigma = ((self.action - mean) - sigma**2) * phi
+        # mean update
+        self.e_mean = self.e_mean * self.lmbd + gradient_mean
+        self.th_mean += self.step_size_mean * self.e_mean * critic_delta
+        # deviation update
+        self.e_sigma = self.e_sigma * lmbda * gamma_next + gradient_sigma
+        self.th_sigma += self.step_size_deviation * self.e_sigma * critic_delta
+
+        mean = np.argmax(np.dot(self.th_mean, phi_next))
+        sigma = np.argmax(np.dot(self.th_sigma, phi_next))
+
+        return np.random.normal(mean,sigma)
--- scripts/pysrc/algorithms/tdcontrol/onpolicy/sarsa.py
+++ scripts/pysrc/algorithms/tdcontrol/onpolicy/sarsa.py
@@ -1,1 +1,35 @@
 __author__ = 'kongaloosh'
+
+from pysrc.algorihtms.tdcontrol.tdcontrol import TDControl
+import numpy as np
+
+__author__ = 'kongaloosh'
+
+
+class SARSA(TDControl):
+    """"""
+
+    def __init__(self, number_of_features, number_of_actions, step_size, active_features=1):
+        """Constructor"""
+        super(ActorCritic, self).__init__(number_of_features, number_of_actions)
+        self.z = np.zeros(self.number_of_actions, self.number_of_features)
+        self.step_size = step_size / active_features
+        self._last_estimate = None
+        self.action = None
+
+    def initialize_episode(self):
+        self.z = np.zeros(self.number_of_features)
+
+    def step(self, phi, reward, phi_next, gamma, lmda, gamma_next):
+        action_next = np.argmax(np.dot(phi_next, self.th))
+        if self.action:
+            delta = reward - self.reaward_avg + gamma_next * np.dot(phi_next, self.th[action_next]) - np.dot(phi, self.th[self.action])
+            self.reaward_avg += self.step_size * delta
+            self.z = gamma * lmda * self.z * (phi == 0.) + (phi != 0.) * phi
+            self.th += self.step_size * delta * self.z
+            self._last_estimate = np.dot(phi_next, self.th[self.action])
+        self.action = action_next
+        return self.action
+
+    def last_estimate(self):
+        return self._last_estimate
